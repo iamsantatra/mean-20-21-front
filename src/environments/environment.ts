9e1e39b77ebe105d586447af10198faf9c53d65a
santatra1999 <81049873+santatra1999@users.noreply.github.com>
2023-06-17 15:21:24 +0300

    fix css and prod environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: "http://localhost:8010/api",
+    // apiUrl: "http://localhost:8010/api",
+    apiUrl: "https://mean-20-21-api.onrender.com/api",
     snackbar: 2000,
     dateFormat: "yyyy-MM-dd",   
 };
