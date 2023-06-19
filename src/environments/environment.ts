69d6e607480a0486c54aadf8276ad1c34fe77c40
santatra1999 <81049873+santatra1999@users.noreply.github.com>
2023-06-19 18:21:11 +0300

    update apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: false,
     // apiUrl: "http://localhost:8010/api",
-    apiUrl: "https://mean-20-21-api.onrender.com/api",
+    // apiUrl: "https://mean-20-21-api.onrender.com/api",
+    apiUrl: "https://assignment-20-21-api.onrender.com/api",
     snackbar: 2000,
     dateFormat: "yyyy-MM-dd",   
 };
